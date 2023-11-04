5f31395c04d35a482efb0b934c76131fc607d0d2
Abhishek Thakare <abhishekthakare@outlook.com>
2023-11-04 18:06:59 -0400

    Made layout with different tabs, and starting getting strings entered into text fields. Lots to do.

--- ProjectThree/src/main/java/com/example/projectthree/HelloController.java
+++ ProjectThree/src/main/java/com/example/projectthree/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.projectthree;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label firstName;
+    @FXML
+    private Label lastName;
+    @FXML
+    private Label dob;
+    @FXML
+    private TextField field1;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Dog!");
+    protected void firstNameAction() {
+        String firstN = field1.getText();
+        System.out.println(firstN);
     }
+    @FXML
+    protected void lastNameAction() {
+        lastName.setText("Done!");
+    }
+    @FXML
+    protected void dobAction() {
+        dob.setText("Done!");
+    }
+
 }
