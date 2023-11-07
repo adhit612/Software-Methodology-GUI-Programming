37f4148e55d7283111c721c52b1eea8bd933686d
Abhishek Thakare <abhishekthakare@outlook.com>
2023-11-06 22:02:13 -0500

    Project Done.

--- ProjectThree/src/main/java/com/example/projectthree/TransactionManagerMain.java
+++ ProjectThree/src/main/java/com/example/projectthree/TransactionManagerMain.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class TransactionManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TransactionManagerMain.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TransactionManagerMain.class.getResource("TransactionManagerView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Project 3 - Transaction Manager");
         stage.setScene(scene);
